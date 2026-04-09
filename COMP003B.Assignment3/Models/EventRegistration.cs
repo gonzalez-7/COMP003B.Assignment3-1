3ec53383ffa097473f5b6b342afd1fc9b5c8599e
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-08 23:05:02 -0700

    Created the Event Registration

---
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+	public class EventRegistration
+	{
+		[Required]
+		[MinLength(3)]
+		public string FullName { get; set; } = string.Empty;
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(20)]
+		public string EventCode { get; set; } = string.Empty;
+
+		[Required]
+		[Range(1, 10)]
+		public int? Tickets { get; set; }
+
+		public string? ReferralCode { get; set; }
+	}
+}
